39ef620f05d3d73f87fa265fceb2952af1b427c3
GreyCoderK <46757795+GreyCoderK@users.noreply.github.com>
2019-05-14 14:24:51 +0000

    ikea, amazon, ali expres, ali baba, zalando scrap
    Pour chaque site ses fonctions commence par le nom du site sauf amazon, les chromes driver c'est pour pouvoir faire la pagination sur ikea il dependent de la version de chrome installer et du systeme d'exploitation que tu possedes installe les chez toi. quand tu vas prendre tu doit changer le path pour que ca marche

--- scarper.py
+++ scarper.py
@@ -3,6 +3,12 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import selenium.webdriver.chrome.service as service
+import itertools
 
 
 base = 'https://www.amazon.fr'
@@ -139,9 +145,9 @@ def paginate_uri(uri, page=1) :
         raise Exception("Veullez entrer une valeur entier")
     elif isinstance(page,int) :
         if uri in get_all_uri(categorie) :
-            return connexion("{uri}&page={page}".format(uri=uri,page=page))
+            return get_octopus_data("{uri}&page={page}".format(uri=uri,page=page))
         elif check_key_exist(uri) :
-            return connexion("{uri}&page={page}".format(uri=get_key_uri(uri),page=page))
+            return get_octopus_data("{uri}&page={page}".format(uri=get_key_uri(uri),page=page))
         else :
             raise Exception("L'url taper n'existe pas")
     else :
@@ -188,6 +194,68 @@ def ikea_categorie() :
             link.append(it['nom'])
     return link
 
+def wait(driver,selector) :
+    WebDriverWait(driver, 3).until( 
+        expected_conditions.presence_of_all_elements_located( 
+            (By.CSS_SELECTOR, selector)
+        ) 
+    )
+    
+
+# def ikea_click_paginate(url) :
+#     results = []
+#     driver = webdriver.Chrome("/Users/armelreal/Downloads/chromedriver")
+#     liens = ikea_lien()
+#     if url in liens :
+#         links = driver.get(url)
+#     elif ikea_get_key_uri(url) in liens:
+#         links = driver.get(ikea_get_key_uri(url))
+#     else:
+#         raise Exception("Erreur mauvaise url")
+
+#     html = connexion(driver.page_source,"div","class","catalog-product-list__total-count")
+#     print(driver.current_url)
+#     # end = int(int(html[-1]) / int(html[1])) - 1
+#     wait(driver,'div.product-compact__spacer')
+    
+#     if driver.find_element_by_css_selector('button.range-btn.catalog-product-list__load-more-button.range-btn--filled.js-load-more__button') and end > 0:      
+#         button = driver.find_element_by_css_selector('button.range-btn.catalog-product-list__load-more-button.range-btn--filled.js-load-more__button')
+#         for i in end:
+#             button.click()
+#             wait(driver,'div.product-compact__spacer')
+#             driver.implicitly_wait(5)
+#             if button.is_displayed() :
+#                 continue
+#             else :
+#                 break
+    
+#     links = connexion(driver.current_url,"div","class","product-compact__spacer")
+
+#     for link in links:
+#         obj = {}
+        
+#         if link.find('img') : 
+#             obj["image"] = link.find('img')['src']
+        
+#         if link.find('span',attrs={'class':"product-compact__name"}) :     
+#             obj["nom"] = link.find('span',attrs={'class':"product-compact__name"}).text.strip()
+        
+#         if link.find('span',attrs={'class':"product-compact__type"}) :             
+#             obj["type"] = link.find('span',attrs={'class':"product-compact__type"}).text.strip()
+        
+#         if link.find('span',attrs={'class':"product-compact__description"}) :                     
+#             obj["description"] = link.find('span',attrs={'class':"product-compact__description"}).text.strip()
+        
+#         if link.find('span',attrs={'class':"product-compact__price"}) :                             
+#             obj["prix"] = link.find('span',attrs={'class':"product-compact__price"}).text.strip()
+
+#         if link.find('span',attrs={'class':"product-compact__ratings"}) :                                 
+#             obj["avis"] = link.find('span',attrs={'class':"product-compact__ratings"}).text.strip()
+        
+#         results.append(obj)
+#     driver.quit()
+#     return results
+
 def ikea_get_article(url) :
     
     results = []
@@ -195,10 +263,12 @@ def ikea_get_article(url) :
 
     if url in liens :
         links = connexion(url,"div","class","product-compact__spacer")
-    elif kea_get_key_uri(url) in liens:
-        links = connexion(kea_get_key_uri(url),"div","class","product-compact__spacer")
+    elif ikea_get_key_uri(url) in liens:
+        links = connexion(ikea_get_key_uri(url),"div","class","product-compact__spacer")
     else :
         raise Exception("L'url recherche n'existe pas")
+    
+    # ikea_click_paginate(url)
 
     for link in links:
         obj = {}
@@ -234,9 +304,156 @@ def ikea_get_key_uri(key) :
     else :
         raise Exception('Veuillez entrer un bon mot clé')
 
+def ali_express_init():
+    url = "https://fr.aliexpress.com"
+    categories = connexion(url,"dl","class","cl-item")
+    return categories
+
+def ali_baba_search_article(categorie,page=1) :
+    els = []
+    url = "https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText={categorie}&page={page}".format(categorie=categorie,page=page)
+    articles = connexion(url,"div","class","m-gallery-product-item-wrap")
+    for item in articles  :
+        result = {}
+        result["image"] = item.find("img")['src']
+        result["libelle"] = item.find("img")['alt']
+        result["prix"] = item.find("b")
+        els.append(result)
+    return els
+
+def ali_express_article(url) :
+    els = []
+    return connexion(url,"div","class","item")
+    if articles == []:
+        articles = connexion(url,"li","class","list-item")
+        print(articles)  
+    for item in articles  :
+        result = {}
+        result["image"] = item.find("img")['src']
+        result["libelle"] = item.find("img")['alt']
+        result["prix"] = item.find("div",attrs={"class":"info"}).find("span",attrs={"class":"value"}).text
+        print(result)
+        els.append(result)
+    return els
+
+def ali_express_format_categorie(categorieHtml) :
+    name = categorieHtml.find('dt',attrs={"class":"cate-name"}).find('a')
+    if name :
+        result = {
+            'nom': name.text,
+            'lien': name['href']
+        }
+        return result
+
+def ali_express_format_souscategorie(categorieHtml) :
+    els = []
+    names = categorieHtml.find('dd',attrs={"class":"sub-cate"}).find('dt').findAll('a')
+    if names :
+        for name in names:
+            if name.text :
+                result = {
+                    'nom': name.text,
+                    'lien': name['href']
+                }
+                els.append(result)
+        return els
+
+def ali_express_format_items(categorieHtml) :
+    els = []
+    names = categorieHtml.find('dd',attrs={"class":"sub-cate"}).find('dd').findAll('a')
+    if names :
+        for name in names:
+            if name.text :
+                result = {
+                    'nom': name.text,
+                    'lien': name['href']
+                }
+                els.append(result)
+        return els
+
+def ali_express_categorie() :
+    return ali_express_categorie_function_caller(1)
+
+
+def ali_express_souscategorie() :
+    return ali_express_categorie_function_caller(2)
+
+
+def ali_express_items() :
+    return ali_express_categorie_function_caller(3)
+
+def ali_express_categorie_function_caller(n = 1) :
+    els = []
+    for item in ali_express_init() :
+        if n == 1 :
+            result = ali_express_format_souscategorie(item)
+        elif n == 2 :
+            result = ali_express_format_categorie(item)
+        elif n == 3 :
+            result = ali_express_format_items(item)
+
+        if result :
+            els.append(result)
+    return els
+
+def list_merge(merge_list):
+    return list(itertools.chain.from_iterable(merge_list))
+
+def ali_express_all():
+    return list_merge(ali_express_items()) + ali_express_souscategorie() + list_merge(ali_express_categorie())
+
+def ali_express_fns(var):
+    result = []
+    for item in ali_express_all() :
+        if item['nom'] not in result :
+            result.append(item[var])
+    return result
+
+def ali_express_keys():
+    return ali_express_fns('nom')
+
+def ali_express_links():
+    return ali_express_fns('lien')
+
+def ali_express_get_url(key):
+    alls = ali_express_all()
+    for item in alls :
+        if key == item['nom']:
+            return item['lien']
+
+def url_paginate_ali_express(url, page = 1):
+    if page > 1:
+        idx = url.index('.html')
+        newUri = "{un}/{page}{deux}".format(un=url[:idx],page=page,deux="?site=fra&tag=")
+        return newUri
+    return url
+
+
+def zalando(typ,categorie,page=1):
+    ret = []
+    if page > 1:
+        url = 'https://www.zalando.fr/{typ}/?q={categorie}&p={page}'.format(typ=typ,categorie=categorie,page=page)
+    else:
+        url = 'https://www.zalando.fr/{typ}/?q={categorie}'.format(typ=typ,categorie=categorie)
+    
+    articles = connexion('https://www.zalando.fr/homme/?q=chaussures&p=3',"z-grid-item","class","cat_articleCard-1r8nF")
+    
+    for item in articles :
+        res = {
+            'images': item.find('img',attrs={'class':'cat_image-1byrW cat_packshotImage-1PSiI'})['srcset'],
+            'nom': item.find('img',attrs={'class':'cat_image-1byrW cat_packshotImage-1PSiI'})['alt'],
+            'prix': item.find('div',attrs={'class':'cat_originalPrice-2Oy4G'}).find('span').text
+        }
+        ret.append(res)
+
+    return ret    
+
 if __name__ == "__main__": 
     
-    links = ikea_get_article('https://www.ikea.com/fr/fr/cat/meubles-pour-lavabo-20719/')
-    for link in links :
-        print(link)
+    # links = ali_express_paginate('Tendance')
+    # print(links)
+    print(zalando("homme",'chaussures',3))
+    # print(connexion("https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText=table&page=1","b"))
+    # for link in new :
+    #     print(link)
     
